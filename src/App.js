27d355e61132ad1d75527135c35cb07621bf32ff
DESKTOP-INOBOOA\Piotr Tobiasz <tobiv1337@gmail.com>
2020-09-27 15:07:57 +0200

    Refactor config file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const App = () => (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
+        Work in progress!
+      </p>
+      <p>
           Edit <code>src/App.js</code> and save to reload.
       </p>
       <a
